ceea84b316a1c2d76ef20b4df26967437fff2a2f
kimpham23 <spham@wesleyan.edu>
2018-07-15 15:59:12 -0400

    testing again

--- DemoProjects/TransitionalApp/app/src/main/java/com/codingwithcasa/matchthecard/LobbyActivity.java
+++ DemoProjects/TransitionalApp/app/src/main/java/com/codingwithcasa/matchthecard/LobbyActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  * test
+ * test 2
  */
 public class LobbyActivity extends Activity {
     @Override

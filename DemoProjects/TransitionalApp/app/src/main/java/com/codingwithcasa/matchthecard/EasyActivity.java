3f8115a0895a2259779e33a11dfc4dd6c58e7a79
kimpham23 <spham@wesleyan.edu>
2018-07-22 15:43:35 -0400

    added buttons to lobby xml and Intent in LobbyActivity

---
+++ DemoProjects/TransitionalApp/app/src/main/java/com/codingwithcasa/matchthecard/EasyActivity.java
@@ -0,0 +1,13 @@
+package com.codingwithcasa.matchthecard;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EasyActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_easy);
+    }
+}
--- DemoProjects/TransitionalApp/app/src/main/java/com/codingwithcasa/matchthecard/LobbyActivity.java
+++ DemoProjects/TransitionalApp/app/src/main/java/com/codingwithcasa/matchthecard/LobbyActivity.java
@@ -1,7 +1,9 @@
 package com.codingwithcasa.matchthecard;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -15,5 +17,9 @@ public class LobbyActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_lobby);
+    }
+    public void easyActivity(View view){
+        Intent intent = new Intent(this, EasyActivity.class);
+        startActivity(intent);
     }
 }

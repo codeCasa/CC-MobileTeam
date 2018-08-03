c9392178a7963b0d664fe941d7bfe0d21e19a436
TreJon House <trejon_house@yahoo.com>
2018-08-03 08:51:08 -0500

    added the count down timer
    Added a timer to countdown from 30secs. This is displayed on a button. Will now proceed to do some UI changes to play with game flow

--- DemoProjects/TransitionalApp/app/src/main/java/com/codingwithcasa/matchthecard/EasyActivity.java
+++ DemoProjects/TransitionalApp/app/src/main/java/com/codingwithcasa/matchthecard/EasyActivity.java
@@ -1,13 +1,67 @@
 package com.codingwithcasa.matchthecard;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class EasyActivity extends Activity {
+import com.codingwithcasa.matchthecard.Utils.CCCache;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class EasyActivity extends Activity implements View.OnClickListener{
+
+    private Button mReady;
+    private Timer mCountDownTimer;
+    private int mReadyTime = 30;
+    private EasyActivity mInstance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_easy);
+        mReady = findViewById(R.id.ReadyBtn);
+        mReady.setOnClickListener(this);
+        mCountDownTimer = new Timer();
+        mInstance = this;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.ReadyBtn:
+                if(mReady.getText().toString().equals(getString(R.string.ready))) {
+                    mReady.setText("" + mReadyTime);
+                    mCountDownTimer.scheduleAtFixedRate(new TimerTask() {
+                        @Override
+                        public void run() {
+                            mReadyTime--;
+                            if (mReadyTime <= 0) {
+                                mInstance.runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        mReady.setText(R.string.reset);
+                                    }
+                                });
+                                mCountDownTimer.cancel();
+                            }else {
+                                mInstance.runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        mReady.setText("" + mReadyTime);
+                                    }
+                                });
+                            }
+                        }
+                    }, 1000, 1000);
+                }else if(mReady.getText().toString().equals(getString(R.string.reset))){
+                    mReady.setText(R.string.ready);
+                    mReadyTime = 30;
+                    mCountDownTimer = new Timer();
+                }
+                break;
+        }
     }
 }
